e4bc37e470d9ab4c71eeeac1942cb56d3d169070
Loup Meurice <loup.meurice@unamur.be>
2020-12-02 15:59:05 +0100

    version générant du code incorrect (transformation d'un dataset<row> en dataset<pojo>)

---
+++ be.unamur.polystore.acceleo/src/be/unamur/polystore/acceleo/main/services/Util.java
@@ -0,0 +1,53 @@
+package be.unamur.polystore.acceleo.main.services;
+
+import be.unamur.polystore.pml.BracketsField;
+import be.unamur.polystore.pml.LongField;
+import be.unamur.polystore.pml.PhysicalField;
+import be.unamur.polystore.pml.TerminalExpression;
+
+public class Util {
+
+	public static String getJavaRegexFromLongField(LongField field) {
+		if (field == null)
+			return null;
+
+		String res = "";
+		for (TerminalExpression expr : field.getPattern()) {
+			if (expr instanceof BracketsField) {
+				res += "(.*)";
+			} else {
+				String literal = expr.getLiteral();
+				literal = escapeReservedCharsInJavaRegex(literal);
+				res += "(" + literal + ")";
+			}
+		}
+		
+		return res;
+	}
+
+	private static String escapeReservedCharsInJavaRegex(String literal) {
+		if (literal == null)
+			return null;
+		return literal.replaceAll("\\$", "\\\\\\$").replaceAll("\\(", "\\\\\\(").replaceAll("\\)", "\\\\\\)"); // TODO
+	}
+	
+	public static String getPositionInLongField(PhysicalField field, LongField parent) {
+		if (field instanceof BracketsField) {
+			BracketsField b = (BracketsField) field;
+			int index = 1;
+			for(TerminalExpression expr : parent.getPattern()) {
+				if(expr instanceof BracketsField) {
+					BracketsField b2 = (BracketsField) expr;
+					if(b.getName().equals(b2.getName()))
+						return "" + index;
+					
+					index++;
+				}
+			}
+		}
+		
+		return null;
+	}
+	
+	
+}
