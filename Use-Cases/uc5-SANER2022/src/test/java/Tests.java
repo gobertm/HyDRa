3ee3dd16a697a21b54b86d719e3c6a6983391fd4
Loup Meurice <loup.meurice@unamur.be>
2021-11-15 16:12:32 +0100

    * jedis mget returns the keys with non-binary value (e.g., hash, list, set, ...) => a test on the value nullity is added * UC5 unit test added: feedback reading

--- Use-Cases/uc5-SANER2022/src/test/java/Tests.java
+++ Use-Cases/uc5-SANER2022/src/test/java/Tests.java
@@ -54,6 +54,15 @@ public class Tests {
         clientOrders.forEach(o -> System.out.println("["+o.getOrderdate()+" - "+o.getTotalprice()+"]"));
         assertEquals(18, orderDataset.count());
     }
+    
+    @Test
+    public void testGetCustomerFeedbacks() {
+    	String prodId = "B003MA1SSI";
+    	Condition condProduct = Condition.simple(ProductAttribute.id, Operator.EQUALS, prodId);
+    	
+    	feedbackDataset = feedbackService.getFeedbackList(condProduct, null, null); 
+    	assertEquals(352, feedbackDataset.count());
+    }
 
     @Test
     public void testInsertOrder() {
