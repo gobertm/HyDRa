719d81f281d3d6c8e0537f997b433cbdb12fe539
Maxime GOBERT <maxime.gobert@unamur.be>
2022-03-23 13:59:49 +0100

    add validation rules

--- be.unamur.polystore/src/be/unamur/polystore/validation/PmlValidator.java
+++ be.unamur.polystore/src/be/unamur/polystore/validation/PmlValidator.java
@@ -3,16 +3,27 @@
  */
 package be.unamur.polystore.validation;
 
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.validation.Check;
 
 import be.unamur.polystore.pml.AbstractMappingRule;
+import be.unamur.polystore.pml.ArrayField;
+import be.unamur.polystore.pml.Attribute;
+import be.unamur.polystore.pml.BracketsField;
+import be.unamur.polystore.pml.Domainmodel;
 import be.unamur.polystore.pml.Edge;
 import be.unamur.polystore.pml.EntityMappingRule;
 import be.unamur.polystore.pml.EntityType;
+import be.unamur.polystore.pml.Identifier;
 import be.unamur.polystore.pml.MappingRules;
 import be.unamur.polystore.pml.PhysicalField;
 import be.unamur.polystore.pml.PmlPackage;
+import be.unamur.polystore.pml.Reference;
+import be.unamur.polystore.pml.RelationshipMappingRule;
 import be.unamur.polystore.pml.RelationshipType;
+import be.unamur.polystore.pml.RoleToKeyBracketsFieldMappingRule;
+import be.unamur.polystore.pml.RoleToReferenceMappingRule;
+import be.unamur.polystore.pml.ShortField;
 
 /**
  * This class contains custom validation rules. 
@@ -56,8 +67,9 @@ public class PmlValidator extends AbstractPmlValidator {
 		}
 	}
 	
-	@Check void checkFieldIsMapped(PhysicalField field, MappingRules rules) {
+	@Check void checkBracketsFieldIsMapped(BracketsField field) {
 		boolean mapped = false;
+		MappingRules rules = EcoreUtil2.getContainerOfType(field, Domainmodel.class).getMappingRules();
 		for(AbstractMappingRule rule : rules.getMappingRules()) {
 			if(rule instanceof EntityMappingRule) {
 				if(((EntityMappingRule) rule).getPhysicalFields().contains(field)) {
@@ -65,9 +77,79 @@ public class PmlValidator extends AbstractPmlValidator {
 					break;
 				}
 			}
+			if(rule instanceof RoleToReferenceMappingRule) {
+				if(((RoleToReferenceMappingRule) rule).getReference().getSourceField().contains(field)) {
+					mapped=true;
+					break;
+				}
+			}
+			if(rule instanceof RelationshipMappingRule) {
+				if(((RelationshipMappingRule) rule).getPhysicalFields().contains(field)) {
+					mapped=true;
+					break;
+				}
+			}
+			if(rule instanceof RoleToKeyBracketsFieldMappingRule) {
+				if(((RoleToKeyBracketsFieldMappingRule) rule).getKeyField().equals(field)) {
+					mapped=true;
+					break;
+				}
+			}
 		}
 		if(!mapped) {
+			warning("This physical field is not mapped in mapping rules section nor in references", PmlPackage.Literals.BRACKETS_FIELD__NAME, INVALID_NAME);
 		}
 	}
 	
+	@Check void checkShortFieldIsMapped(ShortField field) {
+		boolean mapped = false;
+		MappingRules rules = EcoreUtil2.getContainerOfType(field, Domainmodel.class).getMappingRules();
+		for(AbstractMappingRule rule : rules.getMappingRules()) {
+			if(rule instanceof EntityMappingRule) {
+				if(((EntityMappingRule) rule).getPhysicalFields().contains(field)) {
+					mapped = true;
+					break;
+				}
+			}
+			if(rule instanceof RoleToReferenceMappingRule) {
+				if(((RoleToReferenceMappingRule) rule).getReference().getSourceField().contains(field)) {
+					mapped=true;
+					break;
+				}
+			}
+			if(rule instanceof RelationshipMappingRule) {
+				if(((RelationshipMappingRule) rule).getPhysicalFields().contains(field)) {
+					mapped=true;
+					break;
+				}
+			}
+		}
+		if(!mapped) {
+			warning("This physical field is not mapped to anything in mapping rules section nor in references", PmlPackage.Literals.SHORT_FIELD__NAME, INVALID_NAME);
+		}
+	}
+	
+	@Check void checkReferenceIsMapped(Reference reference) {
+		boolean mapped = false;
+		MappingRules rules = EcoreUtil2.getContainerOfType(reference, Domainmodel.class).getMappingRules();
+		for(AbstractMappingRule rule : rules.getMappingRules()) {
+			if(rule instanceof RoleToReferenceMappingRule) {
+				if(((RoleToReferenceMappingRule) rule).getReference().equals(reference)) {
+					mapped=true;
+				}
+			}
+		}
+		if(!mapped) {
+			warning("This reference is not mapped to anything in mapping rules section", PmlPackage.Literals.REFERENCE__NAME, INVALID_NAME);
+		}
+
+	}
+	
+	@Check void checkUpperCaseIdentifier(Identifier identifier) {
+		for(Attribute attr : identifier.getAttributes()) {
+			if(Character.isUpperCase(attr.getName().charAt(0))) {
+				error("Attributes in identifiers cannot start with uppercase letter. Causes fail in HyDRa generated code", PmlPackage.Literals.IDENTIFIER__ATTRIBUTES, INVALID_NAME);
+			}
+		}
+	}
 }
